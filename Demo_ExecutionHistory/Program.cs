b9ba9626d0459f24c68432189d553a1b5d31a530
freeman6 <freeman6@gmail.com>
2019-04-14 19:48:58 +0800

    final

--- Demo_ExecutionHistory/Program.cs
+++ Demo_ExecutionHistory/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSF.AOP;
+using CSF.Common;
 
 namespace Demo_ExecutionHistory
 {
@@ -10,6 +12,32 @@ namespace Demo_ExecutionHistory
     {
         static void Main(string[] args)
         {
+            var obj = ProxyFactory.Create<Calculator>();
+            
+            var result1 = obj.Addition(11, 11);
+            var result2 = obj.Subtraction(10, 11);
+            var result3 = obj.Multiplication(10, 11);
+            var result4 = obj.Division(10, 1);
+            Console.WriteLine($"加法:{result1}\n減法:{result2}\n乘法:{result3}\n除法:{result4}");
+            Console.ReadLine();
         }
+    }
+
+    
+    public class Calculator: MarshalByRefObject
+    {
+        //加法
+        [ExecutionHistory]
+        public int Addition(int x, int y) => x + y;
+        //減法
+        [ExecutionHistory]
+        public int Subtraction(int x, int y) => x - y;
+        //乘法
+        [ExecutionHistory]
+        public int Multiplication(int x, int y) => x * y;
+        //除法
+        [ExecutionHistory]
+        public int Division(int x, int y) => x / y;
+
     }
 }
